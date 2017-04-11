b222e717e14baacdaef53aa1e7cee3121370b49d
George Valtas <grvaltas@gmail.com>
2017-04-11 21:43:47 +0300

    Add files via upload

--- scripts/watch-space/map.js
+++ scripts/watch-space/map.js
@@ -1,4 +1,9 @@
-function loadMap() {
+if (!mapboxgl.supported()) {
+   // alert('Your browser does not support Mapbox GL');
+   map.innerHTML = 'Your browser does not support Mapbox GL,<br><br>Try using another browser';
+   map.style.visibility = 'visible';
+} else {
+  function loadMap() {
   var latitude = document.getElementById('iss_latitude').innerHTML;
   var longitude = document.getElementById('iss_longitude').innerHTML;
   mapboxgl.accessToken = 'pk.eyJ1IjoiZ29nZW9yZ2UiLCJhIjoiY2owOGh1Z3ZmMDAxbTJ3czI5ZWNkNWU0MSJ9.uaq3KZrmE_AHCa_6Ayid7w';
@@ -42,4 +47,5 @@ function loadMap() {
   setTimeout(loadMap, 6000);
 }
 loadMap();
-delete map;
+}
+
