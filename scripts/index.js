e967641bdde0840e85bbc01c0de6ae365935d8a6
George Valtas <grvaltas@gmail.com>
2017-10-29 20:23:15 +0200

    animations onscroll

--- scripts/index.js
+++ scripts/index.js
@@ -5,75 +5,6 @@
 		window.open('https://github.com/gogeorge'); 
 	});
 
-	function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-	}
-
-	window.onclick = function(event) {
-	  if (!event.target.matches('.dropbtn')) {
-	    var dropdowns = document.getElementsByClassName("dropdown-content");
-	    var i;
-	    for (i = 0; i < dropdowns.length; i++) {
-	      var openDropdown = dropdowns[i];
-	      if (openDropdown.classList.contains('show')) {
-	        openDropdown.classList.remove('show');
-	      }
-	    }
-	  }
-	}
-	var img1 = document.getElementById('img1');
-	var img2 = document.getElementById('img2');
-	var img3 = document.getElementById('img3');
-	var proYveete = document.getElementById('pro-yveete');
-	var proAfrodita = document.getElementById('pro-afrodita');
-	var proBash = document.getElementById('pro-bash-profile');
-	var img21 = document.getElementById('img21');
-	var img22 = document.getElementById('img22');
-	var fiverr = document.getElementById('work-fiverr');
-
-	proYveete.onmouseover = function() {
-		img1.style.visibility = "visible";
-	} 
-	proYveete.onmouseout = function() {
-		img1.style.visibility = "hidden";
-	}
-	proAfrodita.onmouseover = function() {
-		img2.style.visibility = "visible";
-	}
-	proAfrodita.onmouseout = function() {
-		img2.style.visibility = "hidden";
-	}
-	fiverr.onmouseover = function() {
-		img22.style.visibility = "visible";
-	}
-	fiverr.onmouseout = function() {
-		img22.style.visibility = "hidden";
-	}
-
-	function myFunction2() {
-    document.getElementById("myDropdown2").classList.toggle("show2");
-	}
-	window.onclick = function(event2) {
-	  if (!event2.target.matches('.dropbtn2')) {
-	    var dropdowns2 = document.getElementsByClassName("dropdown-content2");
-	    var i;
-	    for (i = 0; i < dropdowns2.length; i++) {
-	      var openDropdown2 = dropdowns2[i];
-	      if (openDropdown2.classList.contains('show2')) {
-	        openDropdown2.classList.remove('show2');
-	      }
-	    }
-	  }
-	}
-	//loading images from another site
-	proj_btn.onmouseover = function() {
-		img1.src = "http://georgevaltas.kissr.com/IMG_0441.JPG";
-		img2.src = "http://georgevaltas.kissr.com/nasa front cover landscape.jpg";
-		img3.src = "http://georgevaltas.kissr.com/bash_img.JPG";
-	}
-	work_btn.onmouseover = function() {
-		img22.src = "http://georgevaltas.kissr.com/logo background george.jpg";
-	}
 	//back to top function
 	document.addEventListener('scroll', function() {
     	if (document.body.scrollHeight == document.body.scrollTop + window.innerHeight) {
@@ -103,3 +34,22 @@
 	} else { 
 		confetti.style.visibility = "hidden";
 	}
+
+function scroll() {
+
+	if (window.pageYOffset > 700) {
+		proj_btn.style.opacity = "1"
+		proj_btn.style.fontSize = "45px"
+	}
+	if (window.location.hash == '#projects' || window.pageYOffset > 800) {
+		myDropdown.style.opacity = '1'
+	}
+	if (window.pageYOffset > 1400) {
+		other_btn.style.opacity = "1"
+		other_btn.style.fontSize = "45px"
+	}
+	if (window.location.hash == '#other' || window.pageYOffset > 1500) {
+		myDropdown2.style.opacity = '1'
+	}
+}
+window.onscroll = scroll
